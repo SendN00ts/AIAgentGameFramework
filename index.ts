97e523337f81e76de03a32cd091dbbc76a070fb8
William Lau <55828115+SendN00ts@users.noreply.github.com>
2025-07-11 16:32:55 +0200

    Increase number of text based posts

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ enum ACTIONS {
 }
 
 // Constant for image post probability (45%)
-const IMAGE_POST_PROBABILITY = 0.45;
+const IMAGE_POST_PROBABILITY = 0.7;
 
 // Tracking variables
 let lastPostTime = 0;
